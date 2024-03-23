98472c50b575eea175094271f6b7a212ecede678
terry-u16 <terry-u16@users.noreply.github.com>
2024-03-24 00:44:52 +0900

    二分木の焼きなましを実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use itertools::Itertools;
 #[allow(unused_imports)]
 use proconio::*;
@@ -29,18 +31,18 @@ impl<T: PartialOrd> ChangeMinMax for T {
 struct Input {
     days: usize,
     n: usize,
-    requests: Vec<Vec<i64>>,
+    requests: Vec<Vec<i32>>,
 }
 
 impl Input {
-    const W: i64 = 1000;
+    const W: i32 = 1000;
 
     fn read() -> Self {
         input! {
-            _w: i64,
+            _w: i32,
             days: usize,
             n: usize,
-            requests: [[i64; n]; days],
+            requests: [[i32; n]; days],
         }
 
         Self { days, n, requests }
@@ -49,157 +51,341 @@ impl Input {
 
 fn main() {
     let input = Input::read();
-    let state = State::init(&input);
+    let sizes = dp(&input);
+    let (coords, indices) = build_squares(&sizes);
+    let env = Env::new(input.clone(), indices.clone());
+    let state = State::new(vec![coords; input.days]);
+    let state = annealing(&env, state, 2.9);
+    eprintln!("{}", state.calc_score(&env).unwrap());
 
-    let state = annealing(&input, state, 2.9);
-    state.print(&input);
+    for coords in &state.coords {
+        let mut rects = indices
+            .rects
+            .iter()
+            .map(|r| coords.get_rect(r))
+            .collect_vec();
+        rects.sort_by_key(|r| r.area());
+
+        for rect in rects.iter() {
+            println!("{}", rect);
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Env {
+    input: Input,
+    coord_indices: CoordIndices,
+}
+
+impl Env {
+    fn new(input: Input, coord_indices: CoordIndices) -> Self {
+        Self {
+            input,
+            coord_indices,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
 struct State {
-    lines: Vec<Vec<Option<i64>>>,
+    coords: Vec<LineCoords>,
 }
 
 impl State {
-    fn init(input: &Input) -> Self {
-        let mut lines = vec![];
-
-        for day in 0..input.days {
-            let mut prefix_sum = vec![0];
-
-            for a in input.requests[day].iter() {
-                prefix_sum.push(prefix_sum.last().unwrap() + a);
-            }
-
-            let sum = prefix_sum.last().unwrap();
-
-            let mut l = vec![];
-
-            for (i, s) in prefix_sum.iter().enumerate() {
-                l.push(Some(((s * Input::W + sum / 2) / sum).max(i as i64)));
-            }
-
-            lines.push(l);
-        }
-
-        Self { lines }
+    fn new(coords: Vec<LineCoords>) -> Self {
+        Self { coords }
     }
 
-    fn calc_score(&self, input: &Input) -> Option<i64> {
+    fn calc_score(&self, env: &Env) -> Result<i64, ()> {
         let mut score = 1;
-
-        let mut prev_lines = vec![!0; input.n + 1];
-        let mut prev_lines_sorted = vec![!0; input.n + 1];
-        let mut lines = vec![];
         let mut areas = vec![];
 
-        for (day, (requests, xs)) in input.requests.iter().zip(self.lines.iter()).enumerate() {
-            lines.clear();
+        for (reqs, coord) in env.input.requests.iter().zip(self.coords.iter()) {
             areas.clear();
 
-            for i in 0..=input.n {
-                let x = xs[i];
-                let prev = prev_lines[i];
-
-                let x = match x {
-                    Some(x) => x,
-                    None => prev,
-                };
-
-                prev_lines[i] = x;
-                lines.push(x);
-            }
-
-            glidesort::sort(&mut lines);
-
-            let mut i0 = 0;
-            let mut i1 = 0;
-
-            if day != 0 {
-                while i0 < input.n || i1 < input.n {
-                    let l0 = lines[i0];
-                    let l1 = prev_lines_sorted[i1];
-
-                    if l0 == l1 {
-                        i0 += 1;
-                        i1 += 1;
-                    } else if l0 < l1 {
-                        i0 += 1;
-                        score += Input::W;
-                    } else {
-                        i1 += 1;
-                        score += Input::W;
-                    }
-                }
-            }
-
-            prev_lines_sorted.copy_from_slice(&lines);
-
-            for (x0, x1) in lines.iter().tuple_windows() {
-                let dx = x1 - x0;
-
-                if dx == 0 {
-                    return None;
+            for r in env.coord_indices.rects.iter() {
+                let rect = coord.get_rect(r);
+                if !rect.is_valid() {
+                    return Err(());
                 }
 
-                areas.push(dx * Input::W);
+                let area = rect.area();
+                areas.push(area);
             }
 
             glidesort::sort(&mut areas);
 
-            for (&request, &area) in requests.iter().zip(areas.iter()) {
-                let diff = area - request;
-
-                if diff < 0 {
-                    score -= diff * 100;
-                }
+            for (req, area) in reqs.iter().zip(&areas) {
+                score += 100 * (req - area).max(0) as i64;
             }
         }
 
-        Some(score)
+        // 線分が偶然重なることを考慮していないので、厳密には正しくない
+        for l in env.coord_indices.lines.iter() {
+            let mut l0 = self.coords[0].get_line(l);
+
+            for coord in self.coords.iter().skip(1) {
+                let l1 = coord.get_line(l);
+                score += l0.diff(&l1) as i64;
+                l0 = l1;
+            }
+        }
+
+        Ok(score)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct LineCoords {
+    coords: Vec<i32>,
+}
+
+impl LineCoords {
+    fn new() -> Self {
+        Self { coords: vec![] }
     }
 
-    fn print(&self, input: &Input) {
-        let mut prev_lines = vec![!0; input.n + 1];
-        let mut lines = vec![];
-        let mut pairs = vec![];
+    fn get_line(&self, line: &LineIndex) -> Line {
+        Line::new(
+            self.coords[line.mu0],
+            self.coords[line.mu1],
+            self.coords[line.xi],
+        )
+    }
 
-        for xs in self.lines.iter() {
-            lines.clear();
-            pairs.clear();
+    fn get_rect(&self, rect: &RectIndex) -> Rect {
+        Rect::new(
+            self.coords[rect.x0],
+            self.coords[rect.y0],
+            self.coords[rect.x1],
+            self.coords[rect.y1],
+        )
+    }
+}
 
-            for i in 0..=input.n {
-                let x = xs[i];
-                let prev = prev_lines[i];
+#[derive(Debug, Clone)]
+struct CoordIndices {
+    lines: Vec<LineIndex>,
+    rects: Vec<RectIndex>,
+}
 
-                let x = match x {
-                    Some(x) => x,
-                    None => prev,
-                };
-
-                prev_lines[i] = x;
-                lines.push(x);
-            }
-
-            glidesort::sort(&mut lines);
-
-            for (&x0, &x1) in lines.iter().tuple_windows() {
-                pairs.push((x0, x1));
-            }
-
-            glidesort::sort_by_key(&mut pairs, |(x0, x1)| x1 - x0);
-
-            for (x0, x1) in pairs.iter() {
-                println!("{} {} {} {}", x0, 0, x1, Input::W);
-            }
+impl CoordIndices {
+    fn new() -> Self {
+        Self {
+            lines: vec![],
+            rects: vec![],
         }
     }
 }
 
-fn annealing(input: &Input, initial_solution: State, duration: f64) -> State {
-    let mut solution = initial_solution;
-    let mut best_solution = solution.clone();
-    let mut current_score = solution.calc_score(input).unwrap();
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Line {
+    mu0: i32,
+    mu1: i32,
+    xi: i32,
+}
+
+impl Line {
+    fn new(mu0: i32, mu1: i32, xi: i32) -> Self {
+        Self { mu0, mu1, xi }
+    }
+
+    fn len(&self) -> i32 {
+        self.mu1 - self.mu0
+    }
+
+    fn overlap(&self, other: &Self) -> i32 {
+        if self.xi != other.xi {
+            return 0;
+        }
+
+        let mu0 = self.mu0.max(other.mu0);
+        let mu1 = self.mu1.min(other.mu1);
+        (mu1 - mu0).max(0)
+    }
+
+    fn diff(&self, other: &Self) -> i32 {
+        self.len() + other.len() - 2 * self.overlap(other)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct LineIndex {
+    mu0: usize,
+    mu1: usize,
+    xi: usize,
+}
+
+impl LineIndex {
+    fn new(mu0: usize, mu1: usize, xi: usize) -> Self {
+        Self { mu0, mu1, xi }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Rect {
+    x0: i32,
+    y0: i32,
+    x1: i32,
+    y1: i32,
+}
+
+impl Rect {
+    fn new(x0: i32, y0: i32, x1: i32, y1: i32) -> Self {
+        Self { x0, y0, x1, y1 }
+    }
+
+    fn is_valid(&self) -> bool {
+        self.x0 < self.x1 && self.y0 < self.y1
+    }
+
+    fn area(&self) -> i32 {
+        (self.x1 - self.x0) * (self.y1 - self.y0)
+    }
+}
+
+impl Display for Rect {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {} {}", self.x0, self.y0, self.x1, self.y1)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct RectIndex {
+    x0: usize,
+    y0: usize,
+    x1: usize,
+    y1: usize,
+}
+
+impl RectIndex {
+    fn new(x0: usize, y0: usize, x1: usize, y1: usize) -> Self {
+        Self { x0, y0, x1, y1 }
+    }
+}
+
+fn dp(input: &Input) -> Vec<i32> {
+    const INF: i32 = i32::MAX / 2;
+    const MAX_POW: usize = 10;
+    const SUM: usize = 1 << MAX_POW;
+    let mut dp = vec![vec![INF; SUM + 1]; input.n + 1];
+    let mut from = vec![vec![!0; SUM + 1]; input.n + 1];
+    let sizes = (0..=MAX_POW)
+        .map(|i| (1 << i) * Input::W * Input::W / (1 << MAX_POW))
+        .collect_vec();
+    dp[0][0] = 0;
+
+    for i in 0..input.n {
+        let target = input.requests.iter().map(|r| r[i]).max().unwrap();
+        //let target = input.requests.iter().map(|r| r[i]).sum::<i64>() / input.days as i64;
+
+        for j in 0..=SUM {
+            for pow in 0..=MAX_POW {
+                let next = j + (1 << pow);
+
+                if next > SUM {
+                    break;
+                }
+
+                let next_score = dp[i][j] + (target - sizes[pow]).max(0);
+
+                if dp[i + 1][next].change_min(next_score) {
+                    from[i + 1][next] = j;
+                }
+            }
+        }
+    }
+
+    let mut sizes = vec![];
+    let mut i = input.n;
+    let mut j = SUM;
+
+    while i > 0 {
+        let prev = from[i][j];
+        sizes.push((j - prev) as i32);
+        j = prev;
+        i -= 1;
+    }
+
+    sizes.reverse();
+    eprintln!("best: {}", dp[input.n][SUM]);
+    eprintln!("{:?}", sizes);
+
+    sizes
+}
+
+fn build_squares(sizes: &[i32]) -> (LineCoords, CoordIndices) {
+    let mut index = 0;
+    let mut line_coords = LineCoords::new();
+    let mut coord_indices = CoordIndices::new();
+    line_coords.coords.push(0);
+    line_coords.coords.push(0);
+    line_coords.coords.push(Input::W);
+    line_coords.coords.push(Input::W);
+
+    sq_dfs(
+        0,
+        1,
+        2,
+        3,
+        &mut index,
+        1024,
+        sizes,
+        true,
+        &mut line_coords,
+        &mut coord_indices,
+    );
+
+    (line_coords, coord_indices)
+}
+
+fn sq_dfs(
+    x0: usize,
+    y0: usize,
+    x1: usize,
+    y1: usize,
+    index: &mut usize,
+    size: i32,
+    sizes: &[i32],
+    vertical: bool,
+    cd: &mut LineCoords,
+    idx: &mut CoordIndices,
+) {
+    if sizes[*index] == size {
+        idx.rects.push(RectIndex::new(x0, y0, x1, y1));
+        *index += 1;
+        return;
+    }
+
+    let size = size / 2;
+
+    if vertical {
+        let xx0 = cd.coords[x0];
+        let xx1 = cd.coords[x1];
+        let mid = (xx0 + xx1) / 2;
+        let i = cd.coords.len();
+        cd.coords.push(mid as i32);
+        idx.lines.push(LineIndex::new(y0, y1, i));
+
+        sq_dfs(x0, y0, i, y1, index, size, sizes, !vertical, cd, idx);
+        sq_dfs(i, y0, x1, y1, index, size, sizes, !vertical, cd, idx);
+    } else {
+        let yy0 = cd.coords[y0];
+        let yy1 = cd.coords[y1];
+        let mid = (yy0 + yy1) / 2;
+        let i = cd.coords.len();
+        cd.coords.push(mid as i32);
+        idx.lines.push(LineIndex::new(x0, x1, i));
+
+        sq_dfs(x0, y0, x1, i, index, size, sizes, !vertical, cd, idx);
+        sq_dfs(x0, i, x1, y1, index, size, sizes, !vertical, cd, idx);
+    }
+}
+
+fn annealing(env: &Env, mut state: State, duration: f64) -> State {
+    let mut best_solution = state.clone();
+    let mut current_score = state.calc_score(&env).unwrap();
     let mut best_score = current_score;
     let init_score = current_score;
 
@@ -212,8 +398,8 @@ fn annealing(input: &Input, initial_solution: State, duration: f64) -> State {
     let duration_inv = 1.0 / duration;
     let since = std::time::Instant::now();
 
-    let temp0 = 1e7;
-    let temp1 = 3e2;
+    let temp0 = 1e6;
+    let temp1 = 1e1;
     let mut inv_temp = 1.0 / temp0;
 
     loop {
@@ -229,50 +415,52 @@ fn annealing(input: &Input, initial_solution: State, duration: f64) -> State {
         }
 
         // 変形
-        let day = rng.gen_range(0..input.days);
-        let i = rng.gen_range(1..input.n);
+        let new_state = if rng.gen_bool(0.2) {
+            // copy
+            let pre = rng.gen_bool(0.5);
+            let mut day = rng.gen_range(0..env.input.days - 1);
+            if pre {
+                day += 1;
+            }
 
-        let same_pos = rng.gen_bool(0.2);
+            let index = rng.gen_range(4..state.coords[day].coords.len());
 
-        if same_pos && (day == 0 || solution.lines[day][i].is_none()) {
-            continue;
-        }
+            if pre {
+                if state.coords[day].coords[index] == state.coords[day - 1].coords[index] {
+                    continue;
+                }
 
-        let mut new_solution = solution.clone();
+                let mut new_state = state.clone();
+                new_state.coords[day].coords[index] = new_state.coords[day - 1].coords[index];
+                new_state
+            } else {
+                if state.coords[day].coords[index] == state.coords[day + 1].coords[index] {
+                    continue;
+                }
 
-        if same_pos {
-            new_solution.lines[day][i] = None;
+                let mut new_state = state.clone();
+                new_state.coords[day].coords[index] = new_state.coords[day + 1].coords[index];
+                new_state
+            }
         } else {
-            let old_x = match solution.lines[day][i] {
-                Some(x) => x,
-                None => {
-                    let mut d = day - 1;
+            // shift
+            let day = rng.gen_range(0..env.input.days);
+            let index = rng.gen_range(4..state.coords[day].coords.len());
+            let sign = if rng.gen_bool(0.5) { 1 } else { -1 };
+            let dx = sign * 10f64.powf(rng.gen_range(0.0..2.0)).round() as i32;
+            let new_x = state.coords[day].coords[index] + dx;
 
-                    loop {
-                        if let Some(x) = solution.lines[d][i] {
-                            break x;
-                        }
+            if new_x <= 0 || new_x >= Input::W {
+                continue;
+            }
 
-                        d -= 1;
-                    }
-                }
-            };
-
-            let x = loop {
-                let sign = if rng.gen_bool(0.5) { 1 } else { -1 };
-                let dx = sign * 10.0f64.powf(rng.gen_range(0.0..3.0)).round() as i64;
-                let x = old_x + dx;
-
-                if 0 < x && x < Input::W {
-                    break x;
-                }
-            };
-
-            new_solution.lines[day][i] = Some(x);
-        }
+            let mut new_state = state.clone();
+            new_state.coords[day].coords[index] = new_x;
+            new_state
+        };
 
         // スコア計算
-        let Some(new_score) = new_solution.calc_score(input) else {
+        let Ok(new_score) = new_state.calc_score(&env) else {
             continue;
         };
         let score_diff = new_score - current_score;
@@ -281,10 +469,10 @@ fn annealing(input: &Input, initial_solution: State, duration: f64) -> State {
             // 解の更新
             current_score = new_score;
             accepted_count += 1;
-            solution = new_solution;
+            state = new_state;
 
             if best_score.change_min(current_score) {
-                best_solution = solution.clone();
+                best_solution = state.clone();
                 update_count += 1;
             }
         }
