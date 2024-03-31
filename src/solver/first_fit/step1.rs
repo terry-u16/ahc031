9b982748634cb08828283f405dd41d963337abd5
terry-u16 <terry-u16@users.noreply.github.com>
2024-04-01 01:19:51 +0900

    高速化

--- src/solver/first_fit/step1.rs
+++ src/solver/first_fit/step1.rs
@@ -7,7 +7,7 @@ use rand::prelude::*;
 pub fn generate_init_solution(input: &Input, duration: f64) -> (Vec<i32>, Vec<Vec<(usize, i32)>>) {
     let state = State::new(vec![Input::W], vec![false; input.n]);
     let state = annealing(input, state, duration);
-    let score = state.calc_score(input);
+    let score = state.calc_score(input, i64::MAX);
     eprintln!("first_fit - step1 score: {}", score);
 
     state.restore(input)
@@ -28,7 +28,7 @@ impl State {
         self.widths.len()
     }
 
-    fn calc_score(&self, input: &Input) -> i64 {
+    fn calc_score(&self, input: &Input, threshold: i64) -> i64 {
         let mut widths = self.widths.clone();
         widths.sort_unstable();
 
@@ -92,6 +92,11 @@ impl State {
 
             // 1つ以上入れないとダメ
             score += y_lanes.iter().filter(|&&y| y == 0).count() as i64 * 1000;
+
+            // 閾値以下にならないことが確定したら打ち切り
+            if score > threshold {
+                return 1 << 50;
+            }
         }
 
         score
@@ -197,7 +202,7 @@ impl State {
 fn annealing(input: &Input, initial_solution: State, duration: f64) -> State {
     let mut state = initial_solution;
     let mut best_state = state.clone();
-    let mut current_score = state.calc_score(input);
+    let mut current_score = state.calc_score(input, i64::MAX);
     let mut best_score = current_score;
 
     let mut all_iter = 0;
@@ -208,14 +213,13 @@ fn annealing(input: &Input, initial_solution: State, duration: f64) -> State {
 
     let temp0 = 1e7;
     let temp1 = 1e0;
-    let mut inv_temp = 1.0 / temp0;
+    let mut temp = temp0;
 
     loop {
         all_iter += 1;
         if (all_iter & ((1 << 4) - 1)) == 0 {
             let time = (std::time::Instant::now() - since).as_secs_f64() * duration_inv;
-            let temp = f64::powf(temp0, 1.0 - time) * f64::powf(temp1, time);
-            inv_temp = 1.0 / temp;
+            temp = f64::powf(temp0, 1.0 - time) * f64::powf(temp1, time);
 
             if time >= 1.0 {
                 break;
@@ -289,10 +293,13 @@ fn annealing(input: &Input, initial_solution: State, duration: f64) -> State {
         new_state.widths.sort_unstable();
 
         // スコア計算
-        let new_score = new_state.calc_score(input);
-        let score_diff = new_score - current_score;
+        // 先に閾値を求めることで評価を高速化する
+        let score_threshold =
+            current_score - (temp * rng.gen_range(0.0f64..1.0).ln()).round() as i64;
 
-        if score_diff <= 0 || rng.gen_bool(f64::exp(-score_diff as f64 * inv_temp)) {
+        let new_score = new_state.calc_score(input, score_threshold);
+
+        if new_score <= score_threshold {
             // 解の更新
             current_score = new_score;
             state = new_state;
