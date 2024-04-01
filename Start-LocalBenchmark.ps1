540de0ee4ce569606f168a830aa2b6802e88fba8
terry-u16 <terry-u16@users.noreply.github.com>
2024-04-01 00:26:56 +0000

    linux向けの設定に修正

--- Start-LocalBenchmark.ps1
+++ Start-LocalBenchmark.ps1
@@ -1,8 +1,8 @@
 Write-Host "[Compile]"
 cargo build --release
-Move-Item ../target/release/ahc031.exe . -Force
+Move-Item ./target/release/ahc031 . -Force
 Write-Host "[Run]"
 $env:DURATION_MUL = "1.5"
 $env:AHC030_SHOW_COMMENT = 0
 dotnet marathon run-local
-./relative_score.exe -d ./data/results -o min
+./rel -d ./data/results -o min
